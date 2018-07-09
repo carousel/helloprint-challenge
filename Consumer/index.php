a0980001b2732e05c248092f44d4a07f1cece329
Miroslav Trninic <miroslav.trninic@gmail.com>
2018-07-09 14:05:24 +0200

    update consumer front-controller

--- Consumer/index.php
+++ Consumer/index.php
@@ -3,32 +3,7 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use App\Persistence\DB;
-use App\Persistence\UserRepository;
-use App\Services\Auth;
 use App\Services\Consumer;
 
 $consumer = new Consumer;
-//$user = new UserRepository(new DB);
-//$auth = new Auth($user);
-
-//$callback = function ($msg) use($consumer,$user,$auth){
-    //$data = json_decode($msg->body, true);
-    //$type = $data['type'];
-    //if ($type == 'login') {
-        //$logged = $auth->login($data);
-        //if($logged == false){
-            //$consumer->publish(new PhpAmqpLib\Message\AMQPMessage('User does not exist'));
-        //}
-    //}
-    //if ($type == 'register') {
-        //$user->addNewUser($data);
-    //}
-    //if ($type == 'recovery') {
-        //$user->changePassword($data);
-    //}
-//};
-
-//listen for incoming messages
-//$consumer->listen($callback);
 $consumer->listen();
